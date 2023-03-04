72aece6e9c98a25965ee8d6ef982fb01c6eec1ab
Mofiz Mojib Haider <mojib.haider@therapservices.net>
2023-03-04 06:38:37 +0600

    Added post request using axios

--- react/dojo-blog/src/Component/Create.js
+++ react/dojo-blog/src/Component/Create.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import { useState } from 'react';
+import axios from 'axios'
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
 
-    const[title, setTitle] = useState();
-    const[body, setBody] = useState();
-    const[author, setAuthor] = useState();
+    const[title, setTitle] = useState('');
+    const[body, setBody] = useState('');
+    const[author, setAuthor] = useState('Mojib');
+    const[isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const blog = {title, body, author};
+
+        console.log(blog);
+        setIsPending(true);
+
+        axios.post('http://localhost:8000/blogs/', blog)
+        .then((response) =>{
+            console.log('----------------------');
+            console.log(response);
+            setIsPending(false);  
+            history.push('/');
+        }).catch((error) => {
+            console.log(error.response.data.messages.errors);
+        })
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required/>
 
@@ -22,6 +45,8 @@ const Create = () => {
                 
                 <label>Blog Body:</label>
                 <textarea value={body} onChange={(e) => setBody(e.target.value)} required/>
+
+                <button>Submit</button>
             </form>
         </div>
     )
